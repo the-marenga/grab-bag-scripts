25860288c0a26f8cc4552fdbe2f048629eb5baa0
Marenga <marengamatdrb@gmail.com>
2024-08-05 03:39:56 +0200

    Remove old script

--- enable_br.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script sets up the blu-ray drive on my server to make it available for makemkv to use. Must be run as root
-
-modprobe sg
-chmod a+rw /dev/sg* 
-chmod a+rw /dev/sr0
